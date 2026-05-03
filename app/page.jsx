cd6aa6b3be1ba33e5de3c30eaf7deb9fee386935
meetbhuva07 <meetbhuva0009@gmail.com>
2026-05-03 22:25:25 +0530

    .

--- app/page.jsx
+++ app/page.jsx
@@ -108,88 +108,81 @@ const page = () => {
       </section>
 
       {/* Projects Section  */}
-       <section className="bg-[#1f242d] text-white px-6 md:px-20 py-20">
-      
-      {/* Heading */}
-      <div className="flex items-center justify-between mb-12">
-        <div className="flex items-center gap-4 flex-1 min-w-0">
-          <h2 className="text-2xl sm:text-3xl font-bold whitespace-nowrap">
-            <span className="text-[#C778DD]">#</span>projects
-          </h2>
+      <section className="bg-[#1f242d] text-white px-6 md:px-20 py-20">
+        {/* Heading */}
+        <div className="flex items-center justify-between mb-12">
+          <div className="flex items-center gap-4 flex-1 min-w-0">
+            <h2 className="text-2xl sm:text-3xl font-bold whitespace-nowrap">
+              <span className="text-[#C778DD]">#</span>projects
+            </h2>
 
-          <div className="h-[1.3px] bg-[#C778DD] flex-1 max-w-50 sm:max-w-87.5 md:max-w-92.5 lg:max-w-145 xl:max-w-155"></div>
+            <div className="h-[1.3px] bg-[#C778DD] flex-1 max-w-50 sm:max-w-87.5 md:max-w-92.5 lg:max-w-145 xl:max-w-155"></div>
+          </div>
+
+          <Link
+            href="/works"
+            className="hidden md:block text-sm text-gray-300 hover:text-white"
+          >
+            View all ~~&gt;
+          </Link>
         </div>
 
-        <Link
-          href="/works"
-          className="hidden md:block text-sm text-gray-300 hover:text-white"
-        >
-          View all ~~&gt;
-        </Link>
-      </div>
+        {/* Cards */}
+        <div className="grid md:grid-cols-3 gap-6">
+          {projects.map((item, index) => (
+            <div
+              key={index}
+              className="border border-gray-500 bg-[#282C33] hover:-translate-y-1 transition hover:shadow-[0_0_6px_#C778DD]"
+            >
+              {/* Image */}
+              <div className="relative w-full h-44 border-b border-gray-500">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  fill
+                  className="object-cover"
+                />
+              </div>
 
-      {/* Cards */}
-      <div className="grid md:grid-cols-3 gap-6">
-        {projects.map((item, index) => (
-          <div
-            key={index}
-            className="border border-gray-500 bg-[#282C33] hover:-translate-y-1 transition hover:shadow-[0_0_6px_#C778DD]"
-          >
-            
-            {/* Image */}
-            <div className="relative w-full h-44 border-b border-gray-500">
-              <Image
-                src={item.image}
-                alt={item.title}
-                fill
-                className="object-cover"
-              />
-            </div>
+              {/* Tech */}
+              <div className="border-b border-gray-500 px-4 py-2 text-gray-400 text-sm">
+                {item.tech}
+              </div>
 
-            {/* Tech */}
-            <div className="border-b border-gray-500 px-4 py-2 text-gray-400 text-sm">
-              {item.tech}
-            </div>
+              {/* Content */}
+              <div className="p-4">
+                <h3 className="text-3xl font-semibold mb-3">{item.title}</h3>
 
-            {/* Content */}
-            <div className="p-4">
-              <h3 className="text-3xl font-semibold mb-3">
-                {item.title}
-              </h3>
+                <p className="text-gray-400 mb-5 text-sm">{item.desc}</p>
 
-              <p className="text-gray-400 mb-5 text-sm">
-                {item.desc}
-              </p>
-
-              {/* Button */}
-              <div className="flex gap-3 flex-wrap">
-                {item.link && (
-                  <Link
-                    href={item.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="border border-[#C778DD] px-4 py-2 hover:bg-[#473a54] transition inline-block"
-                  >
-                    {item.btn1}
-                  </Link>
-                )}
+                {/* Button */}
+                <div className="flex gap-3 flex-wrap">
+                  {item.link ? (
+                    <Link
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="border border-[#C778DD] px-4 py-2 hover:bg-[#473a54] transition inline-block"
+                    >
+                      {item.btn1}
+                    </Link>
+                  ) : null}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      {/* Mobile View All */}
-      <div className="mt-8 md:hidden text-center">
-        <Link
-          href="/works"
-          className="text-sm text-gray-300 hover:text-white"
-        >
-          View all ~~&gt;
-        </Link>
-      </div>
-    </section>
-
+        {/* Mobile View All */}
+        <div className="mt-8 md:hidden text-center">
+          <Link
+            href="/works"
+            className="text-sm text-gray-300 hover:text-white"
+          >
+            View all ~~&gt;
+          </Link>
+        </div>
+      </section>
 
       {/* Skills Section */}
       <section className="bg-[#1f242d] text-white px-6 md:px-20 py-20">
